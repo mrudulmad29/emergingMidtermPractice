fafa18a71b8c154373c65a4501647412966fbf47
Mrudul Dubey <mrudulmad29@gmail.com>
2019-02-22 18:12:29 -0500

    practice commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,15 @@
+/*Mrudul Dubey
+300934267
+16 Feb 2019 */
+
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    function Start() {
+        console.log(`%c App Started...`, 
+        "font-size: 20px; color: blue; font-weight: bold");
+    }
+    
+    window.addEventListener("load", Start);
+
+})();
